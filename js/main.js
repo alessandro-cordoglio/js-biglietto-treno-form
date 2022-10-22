fe1c2d41739007e270199754aca6049d12ff9ef2
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-10-22 18:07:55 +0200

    aggiunto form validation

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 "use strict";
 
 /* creo le variabili */
-const journey_km = document.getElementById("journey_km").innerHTML
-const user_age = document.getElementById("user_age").innerHTML
+const user_name = document.getElementById("user_n").value
+const journey_km = document.getElementById("journey_km").value
 const btn_ticket = document.getElementById("ticket_gen")
 const hidden_div = document.querySelector(".hidden__")
 
 let ticket_price = 0.21*journey_km;
 
+/* evento on click */
 btn_ticket.addEventListener('click', function(){
     hidden_div.classList.replace("hidden__", "show__")
 });
 
+function success() {
+    if(document.getElementById("user_n").value==="" || document.getElementById("journey_km").value==="") { 
+           document.getElementById('ticket_gen').disabled = true; 
+       } else { 
+           document.getElementById('ticket_gen').disabled = false;
+       }
+   }
 
 /* condizione che crea il prezzo scontato */
 /* if(user_age<18){
@@ -19,6 +27,9 @@ btn_ticket.addEventListener('click', function(){
 }
 else if(user_age>=65){
     ticket_price = ticket_price - ((ticket_price*40)/100);
-}
+} */
 
-document.getElementById("ticket_price").innerHTML=`Il prezzo del tuo biglietto è ${ticket_price.toFixed(2)}` */
+document.getElementById("traveler_name").innerHTML= user_n 
+
+document.getElementById("carriage").innerHTML= Math.floor(Math.random() * 10) + 1
+document.getElementById("cp_code").innerHTML= Math.floor(Math.random() * 9000 + 1000);
