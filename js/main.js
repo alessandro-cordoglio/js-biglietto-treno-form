1cf2c35a02026f35a84876a1178ed01a4c469a79
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-10-24 10:33:55 +0200

    fixata condizione

--- js/main.js
+++ js/main.js
@@ -20,26 +20,23 @@ btn_ticket.addEventListener('click', function(){
     const journey_km = Number(document.getElementById("km").value)
     const hidden_div = document.querySelector(".hidden__")
     const age = document.getElementById("user_age").value
-    const age_value = age.value
 
     let ticket_price = 0.21*journey_km;
+
     /* funzione per lo sconto */
-    if(age_value=="adult"){
-        document.getElementById("discount").innerHTML= `Biglietto Standard`
-    }
-    else if(age_value=="old"){
+    document.getElementById("discount").innerHTML= `Biglietto Standard`
+    if(age=="old"){
         ticket_price= ticket_price - (ticket_price*0.4)
         document.getElementById("discount").innerHTML= `Sconto Over 65 (40%)`
     }
-    else if(age_value=="minor"){
+    else if(age=="minor"){
         ticket_price= ticket_price - (ticket_price*0.2)
         document.getElementById("discount").innerHTML= `Sconto Minorenni (20%)`
     }
-    
     document.getElementById("traveler_name").innerHTML= user_name 
     document.getElementById("carriage").innerHTML= Math.floor(Math.random() * 10) + 1
     document.getElementById("cp_code").innerHTML= Math.floor(Math.random() * 9000 + 1000);
-    document.getElementById("ticket_price").innerHTML= `${ticket_price}`
+    document.getElementById("ticket_price").innerHTML= `${ticket_price.toFixed(2)}`
     hidden_div.classList.replace("hidden__", "show__")
 });
 
